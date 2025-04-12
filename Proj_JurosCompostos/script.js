0b75ea872e16fd20dc6dcd87f5e8c74a6e05ba52
VictorLucinger <victorhugoqbl@gmail.com>
2025-04-11 21:44:27 -0300

    (VictorLucinger) Inserção de tabela de resultados.

---
+++ Proj_JurosCompostos/script.js
@@ -0,0 +1,27 @@
+function gerarTabuada() {
+    let valorInicial = document.getElementById("val-inic").value;
+    let investMensal = document.getElementById("invest-men").value;
+    let escrita = document.getElementById("select-estilo").value;
+    let cor = document.getElementById("select-cor").value;
+
+    // let linhaTabuada = "";
+
+    // alert(numero + " | " + mult + " | " + escrita + " | " + cor);
+    // document.getElementById("tabuada-resultado").style.color = cor;
+    // if (escrita == "textual") {
+        // for(contagem = 0; contagem <= mult; contagem++) {
+
+            // linhaTabuada += "<div>" + numero + " multiplicado por " + contagem + " é igual a: " + (numero * contagem) + "</div>";
+        // }
+    // } else {
+        // for(contagem = 0; contagem <= mult; contagem++) {
+
+            // linhaTabuada += "<div>" +numero + " x " + contagem + " = " + (numero * contagem) + "</div>";
+        // }
+    // }
+    
+
+    // document.getElementById("section-tabuada").style.display="block";
+    // document.getElementById("tabuada-resultado").innerHTML = linhaTabuada;
+
+}
